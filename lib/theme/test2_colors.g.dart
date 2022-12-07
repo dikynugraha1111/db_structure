f8d08afa4c7a23c24e52da02d46c1aa66dce9a79
Parabeac-Nest <admin@parabeac.com>
2022-12-07 08:11:10 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/test2_colors.g.dart
@@ -0,0 +1,80 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class Test2Colors {
+
+static const Color richBlack = Color(0xff0b090a);
+
+static const Color blackRich = Color(0xff0b090a);
+
+static const Color lightWhite = Color(0xffffffff);
+
+static const Color blackEerieBlack = Color(0xff161a1d);
+
+static const Color blackRichBlack = Color(0xff0b090a);
+
+static const Color lightSilverChalice = Color(0xffb1a7a6);
+
+static const Color lightLightGray = Color(0xffd3d3d3);
+
+static const Color lightCulturedGray = Color(0xfff5f3f4);
+
+static const Color lightWhite = Color(0xffffffff);
+
+static const Color lightAmethystSmoke = Color(0xff9098b1);
+
+static const Color accentTitanWhite = Color(0xffebf0ff);
+
+static const Color accentCloudBurst = Color(0xff223263);
+
+static const Color neutralGrey = Color(0xff9098b1);
+
+static const Color backgroundWhite = Color(0xffffffff);
+
+static const Color neutralLight = Color(0xffebf0ff);
+
+static const Color primaryTopaz = Color(0xff20c39c);
+
+static const Color primarySoftGrey = Color(0xffd1d6e8);
+
+static const Color solitudeBlue = Color(0xffe4e8f0);
+
+static const Color neutralDark = Color(0xff223263);
+
+static const Color primaryRed = Color(0xfffb7181);
+
+static const Color aliceBlue = Color(0xfff7f8fa);
+
+static const Color manateeBlue = Color(0xff8a919b);
+
+static const Color blackPearl = Color(0xff14171c);
+
+static const Color malachiteGreen = Color(0xff0bc64b);
+
+static const Color hitGrey = Color(0xffa5a9a8);
+
+static const Color cinnabarRed = Color(0xffdd3b3b);
+
+static const Color #042445 = Color(0xffffffff);
+
+static const Color primaryBlue = Color(0xff40bfff);
+
+static const Color ◇LightPrimaryColor = Color(0xff0e4da4);
+
+static const Color grey2 = Color(0xffc4c8cf);
+
+static const Color black = Color(0xff14171c);
+
+static const Color black3 = Color(0xff424a57);
+
+static const Color success40 = Color(0xff28a745);
+
+static const Color fill = Color(0xffffffff);
+
+static const Color outline = Color(0xff000000);
+}
---
+++ lib/theme/test2_text_styles.g.dart
@@ -0,0 +1,450 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class Test2TextStyles {
+
+static const TextStyle doubleExtraLargeBold = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraSmallRegular = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraSmallRegular = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraSmallSemiBold = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraSmallBold = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraSmallLight = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle smallRegular = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle smallSemiBold = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle smallBold = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle smallLight = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle normalRegular = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle normalSemiBold = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle normalBold = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle normalLight = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle mediumRegular = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle mediumSemiBold = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle mediumBold = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle mediumLight = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle largeRegular = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle largeSemiBold = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle largeBold = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle largeLight = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraLargeRegular = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraLargeSemiBold = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraLargeBold = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraLargeLight = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle doubleExtraLargeRegular = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle doubleExtraLargeSemiBold = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle doubleExtraLargeLight = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraSmallRegular = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle buttonText = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH4 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyTextNormalRegular = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH5 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle captionNormalregular = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle captionNormalbold = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle extraSmallSemibold = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH6 = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH2 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle normalSemibold = TextStyle(
+  fontSize: 13,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle smallRegular = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle smallBold = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle formTextNormal = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle 12Sp•Caption = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
